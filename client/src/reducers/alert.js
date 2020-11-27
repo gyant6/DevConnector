beaca16429dc2ef0ed7796eedf72e96552509baf
guangyan.tang <guangyan_tang@hotmail.com>
2020-11-27 22:44:27 +0800

    Alert Component & Action Call

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,12 +1,10 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
-const initialState = {
-    id: 1,
-    msg: 'Please log in'
-}
+const initialState = [];
 
 export default function(state = initialState, action) {
     const { type, payload } = action;
+
     switch(type) {
         case SET_ALERT:
             return [...state, payload];
@@ -14,6 +12,5 @@ export default function(state = initialState, action) {
             return state.filter(alert => alert.id !== payload);
         default:
             return state;
-
     }
 }
