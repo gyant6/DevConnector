a12c8361d1d3db65ee14bcb449d9b1a636c9e24a
guangyan.tang <guangyan_tang@hotmail.com>
2020-11-29 03:26:21 +0800

    Delete Education, Experiences & Account

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
-
-const Education = ({ education }) => {
+import { deleteEducation } from '../../actions/profile'
+const Education = ({ education, deleteEducation }) => {
     const educations = education.map(edu => (
         <tr key={edu._id}>
             <td>{edu.school}</td>
@@ -18,7 +18,7 @@ const Education = ({ education }) => {
                 }
             </td>
             <td>
-                <button className='btn btn-danger'>Delete</button>
+                <button onClick={() => deleteEducation(edu._id)} className='btn btn-danger'>Delete</button>
             </td>
         </tr>
     ))
@@ -41,7 +41,8 @@ const Education = ({ education }) => {
 }
 
 Education.propTypes = {
-    education: PropTypes.array
+    education: PropTypes.array,
+    deleteEducation: PropTypes.func.isRequired
 }
 
-export default Education
+export default connect(null, { deleteEducation })(Education)
