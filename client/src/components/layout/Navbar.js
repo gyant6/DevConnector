3ba4567fe15ae13e58907a7c634b24166886228b
guangyan.tang <guangyan_tang@hotmail.com>
2020-11-28 23:01:39 +0800

    Starting On The Dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,8 +8,14 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
+                <Link to="/dashboard">
+                    <i className="fas fa-user"></i> {' '}
+                    <span className="hide-sm">Dashboard</span>
+                </Link>
+            </li>
+            <li>
                 <a onClick={logout} href="#!">
-                    <i className="fas fa-sign-out-alt"></i> {' '}   
+                    <i className="fas fa-sign-out-alt"></i> {' '} 
                     <span className="hide-sm">Logout</span>
                 </a>
             </li>
@@ -22,10 +28,10 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
                 <a href="#!">Developers</a>
             </li>
             <li>
-                <Link to="register">Register</Link>
+                <Link to="/register">Register</Link>
             </li>
             <li>
-                <Link to="login">Login</Link>
+                <Link to="/login">Login</Link>
             </li>
         </ul>
     );
