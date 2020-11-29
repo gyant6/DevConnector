1443b8b01acfdd3b82f5c7a0ff889d58014c7d7a
guangyan.tang <guangyan_tang@hotmail.com>
2020-11-29 15:25:24 +0800

    Amend clear profile reducer to return null profile instead of empty array

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -33,7 +33,7 @@ export default function(state = initialState, action) {
         case CLEAR_PROFILE:
             return {
                 ...state,
-                profile: [],
+                profile: null,
                 repos: [],
                 loading: false
             }
